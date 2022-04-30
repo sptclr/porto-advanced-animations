d1a8586babf60188000e25d8ea911a9ecefa02c1
Fajar Muhamad <sptclr.dev@mail.com>
2022-04-30 11:20:28 +0700

    create home page

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 import Sidebar from '../Sidebar'
 
 import './index.scss'
@@ -7,6 +8,17 @@ const Layout = () => {
   return (
     <>
       <Sidebar />
+      <div className='page'>
+        <span className='tags top-tags'>&lt;body&gt;</span>
+
+        <Outlet />
+
+        <span className='tags bottom-tags'>
+          &lt;/body&gt;
+          <br />
+          <span className='bottom-tag-html'>&lt;/html&gt;</span>
+        </span>
+      </div>
     </>
   )
 }
